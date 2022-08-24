947729132fc1cc2913ee4ba5d9a98d948566039e
rakirs2 <srmylavarapu@microsoft.com>
2022-08-24 11:42:45 -0500

    starter code

---
+++ Template/Hoagie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    internal abstract class Hoagie
+    {
+        void MakeSandwich()
+        {
+
+        }
+    }
+}
--- Template/Program.cs
+++ Template/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+//code based around https://www.youtube.com/watch?v=aR1B8MlwbRI
+
