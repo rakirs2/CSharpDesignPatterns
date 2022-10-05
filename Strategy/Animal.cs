d38d4aac157f994edfcd49a73ab9c76e1018f7ad
rakirs2 <srmylavarapu@microsoft.com>
2022-10-05 15:18:30 -0400

    updatin strat

---
+++ Strategy/Animal.cs
@@ -0,0 +1,11 @@
+﻿namespace Strategy;
+
+public class Animal
+{
+    private String name;
+    private double height;
+    private int weight;
+    private String favoriteFood;
+    private double speed;
+    private String sound;
+}
---
+++ Strategy/Dog.cs
@@ -0,0 +1,10 @@
+﻿using Strategy;
+using Console = System.Console;
+
+internal class Dog : Animal
+{
+    public void digHole()
+    {
+        Console.WriteLine("Digging Furiously");
+    }
+}
